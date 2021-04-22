2b8fb71c661934871650f2dcf11ec71f49c1027f
Dmytro Voloshyniuk <dvoloshinyuk@gmail.com>
2021-04-22 22:28:06 +0300

    Extended reporter app

--- reporter.py
+++ reporter.py
@@ -1,48 +1,68 @@
+import time
 import pymongo
-
-client = pymongo.MongoClient('localhost', 27017)
-db = client.stream_benchmark
-data = db.data
-
-last_row = data.find_one(sort=[( '_id', pymongo.DESCENDING)])
-first_row = data.find_one(sort=[( '_id', pymongo.ASCENDING)])
-sum_agg = data.aggregate([{'$group': {'_id': 1, 'all': { '$sum': '$msg_size' } } } ])
-
-data_size = 0
-
-for r in sum_agg:
-    data_size = r['all']
-    if data_size is not None:
-        break
-
-timing = (last_row['consumer_ts'] - first_row['kafka_ts']) / 1000
-
-max_diff = 0
-
-query = data.aggregate([{'$group': {'_id': 1, "diff": {'$max': {'$abs': {'$subtract' : ['$consumer_ts', "$kafka_ts"]}} } } } ])
-
-for r in query:
-    max_diff = r['diff'] / 1000
-    if max_diff is not None:
-        break
+from matplotlib import pyplot as plt
 
 
-print("{0:.2f} Mb/s".format((data_size / timing / (1024*1024))*8))
-print(f"Max send/accept diff {str(max_diff)} seconds")
+def get_attribute(cursor, attribute):
+    value = None
+    for row in cursor:
+        value = row[attribute]
+        if value is not None:
+            break
+    return value
 
 
+def initialize_mongo(port):
+    mongo_connection = pymongo.MongoClient('localhost', port)
+    database = mongo_connection.stream_benchmark
+    return database.data
 
-#filepath = 'D:/Sources/UCU/Kafka/02-kafka/data/timestamps.csv'
-#
-#columns = ['consumer_ts', 'kafka_ts', 'msg_size']
-#
-#df = pd.read_csv(filepath, names=columns, header=None)
-#
-#timing = (df.iloc[-1]['consumer_ts'] - df.iloc[0]['kafka_ts'])/1000
-#
-#data_size = (df['msg_size'].sum())
-#
-#max_dif = max(df['consumer_ts'] - df['kafka_ts'])/1000
-#
-#print("{0:.2f} Mb/s".format((data_size / timing / (1024*1024))*8))
-#print(f"Max send/accept diff {str(max_dif)} seconds")
+
+if __name__ == "__main__":
+    runs = 0
+
+    max_deltas = []
+    throughput_measures = []
+
+    table = initialize_mongo(port=27017)
+
+    while runs <= 30:
+
+        last_row = table.find_one(sort=[('_id', pymongo.DESCENDING)])
+        first_row = table.find_one(sort=[('_id', pymongo.ASCENDING)])
+        sum_agg = table.aggregate([{'$group': {'_id': 1, 'all': {'$sum': '$msg_size'}}}])
+
+        timing = (last_row['consumer_ts'] - first_row['kafka_ts']) / 1000
+        data_size = get_attribute(sum_agg, 'all')
+
+        get_max_diff = table.aggregate(
+        [{'$group': {'_id': 1, "diff": {'$max': {'$abs': {'$subtract': ['$consumer_ts', "$kafka_ts"]}}}}}])
+
+        max_diff = get_attribute(get_max_diff, 'diff') / 1000
+
+        throughput = (data_size / timing / (1024 * 1024)) * 8
+
+        max_deltas.append(max_diff)
+        throughput_measures.append(throughput)
+
+        runs += 1
+
+        time.sleep(1)
+
+    measures = list(range(1, runs + 1))
+    plt.subplot(2, 1, 1)
+    plt.plot(measures, throughput_measures)
+    plt.title('Kafka metrics')
+    plt.ylabel('Mb/s')
+
+    plt.subplot(2, 1, 2)
+    plt.plot(measures, max_deltas)
+    plt.plot(max_deltas)
+    plt.xlabel('n query')
+    plt.ylabel('Max diff(seconds)')
+
+    plt.show()
+
+    command = input("Do you want to clear the statistics? (y/n): ")
+    if command == "y":
+        table.delete_many({})
