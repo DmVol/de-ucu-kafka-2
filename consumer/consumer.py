017733e589aeb254f0c3dd079fc237fe09931999
Dmytro Voloshyniuk <dvoloshinyuk@gmail.com>
2021-04-22 21:38:40 +0300

    Modified consumer

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,17 +1,17 @@
 import confluent_kafka
 import sys
 import time
-import csv
 import pymongo
 import uuid
 
-process_uuid = uuid.uuid1()
+process_uuid = uuid.uuid1().hex[:8]
+
+# Add sleep while worker is starting
+time.sleep(10)
 
 client = pymongo.MongoClient('mongo', 27017)
 db = client.stream_benchmark
 data = db.data
-
-#filepath = 'D:/Sources/UCU/Kafka/02-kafka/data/timestamps.csv'
 
 conf = {'bootstrap.servers': "broker:29092",
         'group.id': 'mygroup',
@@ -37,10 +37,5 @@ while True:
     row = {"process": process_uuid, "consumer_ts": int(time.time() * 1000), "kafka_ts": msg.timestamp()[1], "msg_size": sys.getsizeof(msg.value())}
 
     data.insert_one(row)
-    #with open(filepath, 'a', newline='') as file:
-    #    writer = csv.writer(file)
-    #    writer.writerow([int(time.time() * 1000), msg.timestamp()[1], sys.getsizeof(msg.value())])
-
-    #print(msg.value())
 
 consumer.close()
