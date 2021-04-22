3802ef54bc78cb56fc03c60500c426474d2cb1de
Dmytro Voloshyniuk <dvoloshinyuk@gmail.com>
2021-04-22 21:38:25 +0300

    Added docker configs to producer

--- producer/producer.py
+++ producer/producer.py
@@ -2,10 +2,10 @@ import confluent_kafka
 import socket
 import csv
 import json
-import sys
 
-filepath = 'D:/Sources/UCU/Kafka/02-kafka/data/reddit_dataset.csv'
-conf = {'bootstrap.servers': "localhost:9092",
+filepath = 'reddit_comments_10k.csv'
+
+conf = {'bootstrap.servers': "broker:29092",
         'client.id': socket.gethostname()}
 
 producer = confluent_kafka.Producer(conf)
