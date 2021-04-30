29de228d484f4176ea3c63f24b5aec2cc224a5e0
Dmytro Voloshyniuk <dvoloshinyuk@gmail.com>
2021-04-30 22:08:35 +0300

    Fixed kafka producer

--- producer/producer.py
+++ producer/producer.py
@@ -4,7 +4,7 @@ import csv
 import json
 import time
 
-filepath = 'reddit_comments_10k.csv'
+filepath = 'reddit_dataset.csv'
 
 conf = {'bootstrap.servers': "broker:29092",
         'client.id': socket.gethostname()}
@@ -14,7 +14,9 @@ producer = confluent_kafka.Producer(conf)
 with open(filepath, encoding="utf8") as file:
     reader = csv.DictReader(file)
     for row in reader:
+        producer.poll(0)
         row['producer_timestamp'] = int(time.time() * 1000)
         json_data = json.dumps(row).encode('utf-8')
         producer.produce(topic="kafka-test", value=json_data)
-        producer.flush()
+
+producer.flush()
